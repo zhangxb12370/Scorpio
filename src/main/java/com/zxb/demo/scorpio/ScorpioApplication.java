0a3ff95cda757a4d50a89e81ca56304236d5661f
Your Name <2104605213@qq.com>
2018-12-25 11:15:04 +0800

    first commit

---
+++ src/main/java/com/zxb/demo/scorpio/ScorpioApplication.java
@@ -0,0 +1,14 @@
+package com.zxb.demo.scorpio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorpioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScorpioApplication.class, args);
+    }
+
+}
+
